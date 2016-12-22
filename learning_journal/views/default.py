a50e03eec3747825d23fc41c3025a35a8f761a93
CLAIRE GATENBY <clairejgatenby@gmail.com>
2016-12-21 22:26:00 -0800

    updated new and edit functions, now possible to add new entry using html form

--- learning_journal/views/default.py
+++ learning_journal/views/default.py
@@ -1,34 +1,73 @@
+"""Views for learning journal web page."""
+
 from pyramid.response import Response
 from pyramid.view import view_config
 
 from sqlalchemy.exc import DBAPIError
 
 from ..models import MyModel
+from datetime import datetime
 import os
 
 HERE = os.path.dirname(__file__)
 
 
 @view_config(route_name='home', renderer='../templates/list.jinja2')
-def my_view(request):
+def home_page(request):
     """View for home page."""
     try:
         query = request.dbsession.query(MyModel)
         all_entries = query.all()
+        return {'entries': all_entries}
     except DBAPIError:
         return Response(db_err_msg, content_type='text/plain', status=500)
-    return {'entries': all_entries}
 
 
 @view_config(route_name='detail', renderer='../templates/detail.jinja2')
-def detail(request):
-    """Send individual entry for detail view."""
-    query = request.dbsession.query(MyModel)
-    data = query.filter_by(id=request.matchdict['id']).one()
-    return {'entries': data}
+def detail_page(request):
+    """One entry for detail veiw."""
+    try:
+        query = request.dbsession.query(MyModel)
+        data = query.filter_by(id=request.matchdict['id']).one()
+        return {'entries': data}
+    except DBAPIError:
+        return Response(db_err_msg, content_type='text/plain', status=500)
 
 
+@view_config(route_name='edit', renderer='../templates/edit.jinja2')
+def edit_page(request):
+    """Edit page for one entry."""
+    try:
+        query = request.dbsession.query(MyModel)
+        data = query.filter_by(id=request.matchdict['id']).one()
+        # if request.method == "POST":
 
+        #     new_title = request.POST["title"]
+        #     new_body = request.POST["body"]
+        #     new_model = MyModel(title=new_title, value=new_body)
+
+        #     request.dbsession.add(new_model)
+        return {'entries': data}
+    except DBAPIError:
+        return Response(db_err_msg, content_type='text/plain', status=500)
+
+
+@view_config(route_name='new', renderer='../templates/new.jinja2')
+def new_entry(request):
+    """View the new entry page."""
+    try:
+        query = request.dbsession.query(MyModel)
+        all_entries = query.all()
+        if request.method == "POST":
+            date = datetime.now()
+            new_model = MyModel(title=request.POST['title'],
+                                body=request.POST['body'],
+                                creation_date="{}/{}/{}".format(date.month, date.day, date.year)
+                                )
+            request.dbsession.add(new_model)
+        return {'entries': all_entries}
+    except DBAPIError:
+        return Response(db_err_msg, content_type='text/plain', status=500)
 
 
 
