c1329bca1cd9ccf605c1dfec420c84435c0e916f
CLAIRE GATENBY <clairejgatenby@gmail.com>
2016-12-23 11:50:59 -0800

    changed database to envronment variable

--- learning_journal/scripts/initializedb.py
+++ learning_journal/scripts/initializedb.py
@@ -92,7 +92,9 @@ def main(argv=sys.argv):
     config_uri = argv[1]
     options = parse_vars(argv[2:])
     setup_logging(config_uri)
+    
     settings = get_appsettings(config_uri, options=options)
+    settings["sqlalchemy.url"] = os.environ["DATABASE_URL"]
 
     engine = get_engine(settings)
 
