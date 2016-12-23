09e244b41ae947e65650ddda8450725d4ad05464
CLAIRE GATENBY <clairejgatenby@gmail.com>
2016-12-23 13:49:42 -0800

    removed the drop table

--- learning_journal/scripts/initializedb.py
+++ learning_journal/scripts/initializedb.py
@@ -98,7 +98,7 @@ def main(argv=sys.argv):
 
     engine = get_engine(settings)
 
-    Base.metadata.drop_all(engine)
+    # Base.metadata.drop_all(engine)
     Base.metadata.create_all(engine)
 
     session_factory = get_session_factory(engine)
