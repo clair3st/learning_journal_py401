e4e0b99b91da6b3f4c6be2365cda547fcb29349c
CLAIRE GATENBY <clairejgatenby@gmail.com>
2016-12-29 21:13:57 -0800

    added new functional tests from step3

--- learning_journal/tests.py
+++ learning_journal/tests.py
@@ -40,8 +40,8 @@ def configuration(request):
 @pytest.fixture()
 def db_session(configuration, request):
     """Create a session for interacting with the test database."""
-    SessionFactory = configuration.registry['dbsession_factory']
-    session = SessionFactory()
+    session_factory = configuration.registry['dbsession_factory']
+    session = session_factory()
     engine = session.bind
     Base.metadata.drop_all(engine)
     Base.metadata.create_all(engine)
@@ -111,8 +111,8 @@ def testapp():
     })
     testapp = TestApp(app)
 
-    SessionFactory = app.registry["dbsession_factory"]
-    engine = SessionFactory().bind
+    session_factory = app.registry["dbsession_factory"]
+    engine = session_factory().bind
     Base.metadata.create_all(bind=engine)
 
     return testapp
@@ -121,10 +121,18 @@ def testapp():
 @pytest.fixture
 def fill_the_db(testapp):
     """Test fixture for a full db."""
-    SessionFactory = testapp.app.registry["dbsession_factory"]
+    session_factory = testapp.app.registry["dbsession_factory"]
     with transaction.manager:
-        dbsession = get_tm_session(SessionFactory, transaction.manager)
-        dbsession.add_all(MODEL_ENTRIES)
+        dbsession = get_tm_session(session_factory, transaction.manager)
+        for entry in ENTRIES:
+            row = MyModel(
+                title=entry['title'],
+                body=entry['body'],
+                creation_date=datetime.datetime.strptime(entry['creation_date'],
+                                                         '%b %d, %Y')
+            )
+
+            dbsession.add(row)
 
 
 def test_home_route_has_no_article_when_db_empty(testapp):
@@ -146,3 +154,62 @@ def test_new_route_has_form(testapp):
     response = testapp.get('/journal/new-entry', status=200)
     html = response.html
     assert len(html.find_all('form')) == 1
+
+
+def test_detail_page_loads_correct_entry(testapp, fill_the_db):
+    """Test that the detail route loads the proper entry."""
+    response = testapp.get('/journal/1', status=200)
+    title = response.html.find_all(class_='entrytitle')[0].contents[0]
+    assert title == ENTRIES[0]['title']
+
+
+def test_create_view_post_redirects(testapp):
+    """Test that a post request redirects to home.
+
+    reference:
+    http://stackoverflow.com/questions/10773404/how-to-follow-pyramid-redirect-on-tests
+    """
+    post_params = {
+        'title': 'Learning Journal Title',
+        'body': 'So many things learned today.'
+    }
+
+    response = testapp.post('/journal/new-entry', post_params, status=302)
+    full_response = response.follow()
+
+    assert len(full_response.html.find_all(id="journal-entry")) == 1
+
+
+def test_new_entry_adds_to_list(testapp):
+    """Test that new entry page adds to the list view."""
+    post_params = {
+        'title': 'Learning Journal Title',
+        'body': 'So many things learned today.'
+    }
+
+    response = testapp.post('/journal/new-entry', post_params, status=302)
+    full_response = response.follow()
+    assert full_response.html.find(id='journal-entry').a.text == post_params['title']
+
+
+def test_edit_page_redirects_to_home(testapp, fill_the_db):
+    """Test the edit page redirects to home after submit."""
+    post_params = {
+        'id': 1,
+        'title': 'Learning Journal Title',
+        'body': 'So many things learned today.'
+    }
+    response = testapp.post('/journal/1/edit-entry', post_params, status=302)
+    full_response = response.follow()
+
+    assert full_response.html.find_all(class_='entrytitle')[0].text[1:-1] == post_params['title']
+
+
+def test_edit_has_populated_form(testapp, fill_the_db):
+    """Test edit page has a pre-populated form."""
+    response = testapp.get('/journal/1/edit-entry', status=200)
+    title = response.html.form.input["value"]
+    body = response.html.form.textarea.contents[0]
+    print(body)
+    assert title == ENTRIES[0]["title"]
+    assert body == ENTRIES[0]["body"]
