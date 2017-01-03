b13d79dedf81383b3b4e4334f8f7597204a0cc2a
CLAIRE GATENBY <clairejgatenby@gmail.com>
2017-01-02 18:35:11 -0800

    added route for login and logout

--- learning_journal/routes.py
+++ learning_journal/routes.py
@@ -4,3 +4,5 @@ def includeme(config):
     config.add_route('new', '/journal/new-entry')
     config.add_route('detail', '/journal/{id:\d+}')
     config.add_route('edit', '/journal/{id:\d+}/edit-entry')
+    config.add_route('login', '/journal/login')
+    config.add_route('logout', '/journal/logout')
