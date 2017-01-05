8e33d84b8b6e129b4dc50bce590a8a757dc02cb6
CLAIRE GATENBY <clairejgatenby@gmail.com>
2017-01-04 19:29:56 -0800

    created app.js

---
+++ learning_journal/static/app.js
@@ -0,0 +1,28 @@
+
+// $(document).ready(function(){
+//     // var deleters = $(".delete");
+//     // deleters.on("click", function(){
+//     //     // send ajax request to delete this expense
+//     //     $.ajax({
+//     //         url: 'delete/' + $(this).attr("data"),
+//     //         data: {
+//     //             "item": "some name",
+//     //             "paid_to": "some company"
+//     //         }
+//     //         success: function(){
+//     //             console.log("deleted");
+//     //         }
+//     //     });        
+        
+//     // });
+
+//     var csrfToken = ${request.session.get_csrf_token()};
+//     $.ajax({
+//         type: "POST",
+//         url: "/journal/new",
+//         headers: { 'X-CSRF-Token': csrfToken }
+//         }).done(function() {
+//             alert("Ajaxed it");
+//         });
+
+// });
