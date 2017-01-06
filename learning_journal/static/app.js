113d6b01b6b0bbd075956d5471b7db1b3ec13798
CLAIRE GATENBY <clairejgatenby@gmail.com>
2017-01-06 09:38:14 -0800

    added csrf header

--- learning_journal/static/app.js
+++ learning_journal/static/app.js
@@ -1,11 +1,13 @@
 
 $(document).ready(function(){
+    var csrfToken = "{{request.session.get_csrf_token()}}"
     var form_submit = $("#create");
     form_submit.on("click", function(e){
 
         console.log('in function')
         $.ajax({
             url: "/create-home",
+            headers: { 'X-CSRF-Token': csrfToken },
             data: {
                 "title": $("[name='title']").val(),
                 "body": $("[name='body']").val(),
