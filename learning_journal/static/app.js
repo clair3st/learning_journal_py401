5450f10d3459b627f5e220d01ec3b7060d064bdd
CLAIRE GATENBY <clairejgatenby@gmail.com>
2017-01-05 22:50:37 -0800

    updated ajax to not reload

--- learning_journal/static/app.js
+++ learning_journal/static/app.js
@@ -1,7 +1,8 @@
 
 $(document).ready(function(){
     var form_submit = $("#create");
-    form_submit.on("click", function(){
+    form_submit.on("click", function(e){
+
         console.log('in function')
         $.ajax({
             url: "/create-home",
@@ -11,18 +12,29 @@ $(document).ready(function(){
             },
             success: function(){
                 console.log("updated");
+
+                // Render info to list
+                var heading = $('#list-title')
+                var title = $("[name='title']").val()
+                var text = '<article><h2 class="entrytitle" id="journal-entry"><a>'
+                var text2 = '</a></h2><p class="date">'
+                var date = new Date()
+                var date = date.toDateString().slice(4)
+                var date_output = [date.slice(0, 6), ',', date.slice(6)].join('');
+
+                var text3 = '</p></article>'
+                heading.after(text + title + text2 + date_output + text3)
+
+                // Clear form
+                $("[name='title']").val('')
+                $("[name='body']").val('')
+
             }
-        });        
+        
+        }); 
+
+        e.preventDefault();    
         
     });
-
-//     var csrfToken = ${request.session.get_csrf_token()};
-//     $.ajax({
-//         type: "POST",
-//         url: "/journal/new",
-//         headers: { 'X-CSRF-Token': csrfToken }
-//         }).done(function() {
-//             alert("Ajaxed it");
-//         });
 
 });
