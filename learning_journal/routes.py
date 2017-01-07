5b8050fe15a2db25502836a05027701a9cc257cf
CLAIRE GATENBY <clairejgatenby@gmail.com>
2017-01-06 17:37:12 -0800

    added route for getting item by title

--- learning_journal/routes.py
+++ learning_journal/routes.py
@@ -1,9 +1,14 @@
+"""Route to configure for pyramid app."""
+
+
 def includeme(config):
+    """Include routes."""
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
     config.add_route('new', '/journal/new-entry')
     config.add_route('detail', '/journal/{id:\d+}')
     config.add_route('edit', '/journal/{id:\d+}/edit-entry')
     config.add_route('login', '/journal/login')
+    config.add_route('post_by_title', '/journal/{title:[\w\s]+}')
     config.add_route('logout', '/journal/logout')
     config.add_route('delete', '/delete/{id:\d+}')
